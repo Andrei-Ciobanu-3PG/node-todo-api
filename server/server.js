1fd8813b57cf2ef1e27bcd4c104b181335d35fb5
Andrei Ciobanu <andrei.ciobanu@3pillarglobal.com>
2018-09-05 15:03:33 +0300

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -116,7 +116,6 @@ app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
 });
 
-// POST /users/login (email, password)
 app.post('/users/login', (req, res) => {
     const body = _.pick(req.body, ['email','password']);
     User.findByCredentials(body.email, body.password).then((user) => {
@@ -126,6 +125,14 @@ app.post('/users/login', (req, res) => {
     }).catch((err) => {
         res.status(400).send();
     });
+});
+
+app.delete('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(() => {
+        res.status(200).send();
+    }, () => {
+        res.status(400).send();
+    })
 });
 
 app.listen(port, () => {
